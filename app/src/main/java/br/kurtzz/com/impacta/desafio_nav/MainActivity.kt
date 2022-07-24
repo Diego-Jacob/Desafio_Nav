48fa8f24aac9820a4f944d9385c13a8309c76bcb
Diego Jacob <djnovaes@gmail.com>
2022-07-24 15:46:39 -0300

    Layouts Prontos

--- app/src/main/java/br/kurtzz/com/impacta/desafio_nav/MainActivity.kt
+++ app/src/main/java/br/kurtzz/com/impacta/desafio_nav/MainActivity.kt
@@ -2,10 +2,15 @@ package br.kurtzz.com.impacta.desafio_nav
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.kurtzz.com.impacta.desafio_nav.databinding.ActivityMainBinding
+
+var _binding: ActivityMainBinding? = null
+val binding get() = _binding!!
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
--- app/src/main/java/br/kurtzz/com/impacta/desafio_nav/segundo_Fragment.kt
+++ app/src/main/java/br/kurtzz/com/impacta/desafio_nav/segundo_Fragment.kt
@@ -5,55 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import br.kurtzz.com.impacta.desafio_nav.databinding.FragmentSegundoBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [segundo_Fragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class segundo_Fragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentSegundoBinding? = null
+    private val binding: FragmentSegundoBinding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_segundo_, container, false)
+        _binding = FragmentSegundoBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment segundo_Fragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            segundo_Fragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.buttonSegundoFrag.setOnClickListener {
+            findNavController().navigate(R.id.action_segundo_Fragment_to_primeiro_Fragment2)
+        }
     }
-}
+
+
+    }
